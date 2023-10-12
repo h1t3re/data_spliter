1c1da18cbc093801be03f48674316e3112b27196
h1t3re <143657823+h1t3re@users.noreply.github.com>
2023-10-12 23:40:04 +0200

    Add files via upload

--- mem/mem.c
+++ mem/mem.c
@@ -1,21 +1,9 @@
 #include <stdlib.h>
-#include <string.h>
-
-int cmp(int length, char *str1, char *str2)
-{
-	int i = 0;
-	while(i < length)
-	{
-		if(str1[i] != str2[i])
-			return 1;
-		i = i +1;
-	}
-	return 0;
-}
+#include "strncmp.h"
 
 void *mem_remem_array(void *array, char *type, int size)
 {
-	if(cmp(6, type, "char *") == 0)
+	if(strncmp(6, type, "char *") == 0)
 	{
 		if((array == NULL) | (size == 0))
 		{
@@ -34,7 +22,7 @@ void *mem_remem_array(void *array, char *type, int size)
 				array = tmp;
 			}
 		}
-	}else if(cmp(6, type, "char **") == 0)
+	}else if(strncmp(6, type, "char **") == 0)
 	{
 		if((array == NULL) | (size == 0))
 		{
@@ -57,7 +45,7 @@ void *mem_remem_array(void *array, char *type, int size)
 
 void free_array(void *array, char *type, int size)
 {
-	if((cmp(6, type, "char **") == 0) | (strcmp(type, "int **") == 0))
+	if((strncmp(6, type, "char **") == 0) | (strcmp(type, "int **") == 0))
 	{
 		for(int i = 0; i < size; i++)
 			free(*(char *)&array[i]);
