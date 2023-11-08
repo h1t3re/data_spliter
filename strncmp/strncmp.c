883d7a7d6c444bd6c22932b08055e80260f87fdb
h1t3re <143657823+h1t3re@users.noreply.github.com>
2023-11-08 15:07:00 +0100

    Add files via upload

--- strncmp/strncmp.c
+++ strncmp/strncmp.c
@@ -7,5 +7,7 @@ int strncmp(const char *str1, const char *str2)
 			return 1;
 		i = i +1;
 	}
+	if(((*(str1+i) != '\0') & (*(str2+i) == '\0')) | ((*(str1+i) == '\0') & (*(str2+i) != '\0')))
+	       return 1;	
 	return 0;
 }
